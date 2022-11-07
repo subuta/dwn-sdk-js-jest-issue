eedf4f931aaaccd6b82b602972e833cda9109a7b
subuta <shunta.3110@gmail.com>
2022-11-07 16:57:03 +0900

    Fix main script.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { DWN } from '@tbd54566975/dwn-sdk-js';
+import { Dwn } from '@tbd54566975/dwn-sdk-js';
 
 const main = async () => {
     console.log(`Instantiating DWN...`);
-    const dwn = await DWN.create({});
+    const dwn = await Dwn.create({});
 
     console.log(dwn)
 }
